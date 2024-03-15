f37bf6682baced2df293e0d6461703fb9a26cdd0
s28312 <s28312@pjwstk.edu.pl>
2024-03-15 16:44:28 +0100

    Task 3 is done (class)

--- pythonProject1/lab_3.py
+++ pythonProject1/lab_3.py
@@ -1,13 +1,32 @@
-#Task1
+# Task1
 squares = [x ** 2 for x in range(1, 11)]
 
 print(squares)
+
+
 def generate_squares(start, end):
     squares = [x ** 2 for x in range(start, end + 1)]
     return squares
 
+
+# Task2
 start_num = 1
 end_num = 10
 squares_list = generate_squares(start_num, end_num)
-print("List of squares from" ,(start_num, end_num))
+print("List of squares from", (start_num, end_num))
 print(squares_list)
+
+
+# Task3
+
+class SquareGenerator:
+    def __init__(self):
+        self.start = int(input("Enter start: "))
+        self.ends = int(input("Enter end: "))
+
+        squares = [x ** 2 for x in range(self.start, self.ends)]
+        print("Squares:", squares)
+
+start_class = SquareGenerator()
+
+#Task 4
