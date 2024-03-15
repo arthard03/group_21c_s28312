00b1475123037b8fe01e1dc2ddbf82f9a857e495
s28312 <s28312@pjwstk.edu.pl>
2024-03-15 16:31:52 +0100

    Task 2 is done ( with start and end point)

--- pythonProject1/lab_3.py
+++ pythonProject1/lab_3.py
@@ -2,3 +2,12 @@
 squares = [x ** 2 for x in range(1, 11)]
 
 print(squares)
+def generate_squares(start, end):
+    squares = [x ** 2 for x in range(start, end + 1)]
+    return squares
+
+start_num = 1
+end_num = 10
+squares_list = generate_squares(start_num, end_num)
+print("List of squares from" ,(start_num, end_num))
+print(squares_list)
