b49c2a823fc0a9f52e91f2c65cee96f0819d7e43
s28312 <s28312@pjwstk.edu.pl>
2024-03-17 13:13:19 +0100

    Task 9 is done

--- pythonProject1/square_generator/generator.py
+++ pythonProject1/square_generator/generator.py
@@ -7,6 +7,7 @@ import numpy as np
 # Task 6
 # Task 7
 # Task 8
+# Task 9
 class SquareGenerator:
     def __init__(self):
         try:
@@ -29,13 +30,20 @@ class SquareGenerator:
 class CubicGenerator(SquareGenerator):
     def __init__(self):
         super().__init__()
+    def generate_squares(self):
+        if self.start > self.end:
+            print("Error: Start value must smaller or equal.")
+            return None
+        # cubes = [x ** 3 for x in range(self.start, self.end)]
+        squares = [x ** 2 for x in range(self.start, self.end)]
+        print("Squares(from cubic class): ", squares)
+        # print("Cubes: ", cubes)
 
-    def generate_cubes(self):
-        cubes = [x ** 3 for x in range(self.start, self.end)]
-        print("Cubes: ", cubes)
-
-
-start_class = SquareGenerator()
+        return squares
 
 cube_generator = CubicGenerator()
-cube_generator.generate_cubes()
+
+result = cube_generator.generate_squares()
+
+if result is None:
+    print("Squares generation failed.")
